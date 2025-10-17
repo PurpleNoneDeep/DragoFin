f575a4363aa0680e01e9ff9d7b11816516fdea3d
MrzMargo <106422831+MrzMargo@users.noreply.github.com>
2025-10-17 15:30:00 +0300

    add transaction page

--- dragofin/main/views.py
+++ dragofin/main/views.py
@@ -13,6 +13,7 @@ def dashboard(request):
     print(request.user)
     query = request.GET.get('category')
     transactions = Transaction.objects.all()
+    categories = Category.objects.all()
     users = User.objects.all()
     user = users.first()
     if query:
@@ -25,31 +26,45 @@ def delete_transaction(request, transaction_id):
     return redirect('dashboard')
 
 def add_transaction(request):
+    categories = Category.objects.all().values('name', 'type')  # Получаем все категории
+
     if request.method == 'POST':
-        # Получаем данные из формы
         date = request.POST.get('date')
         transaction_type = request.POST.get('type')
         category_name = request.POST.get('custom_category') if request.POST.get('category') == 'custom' else request.POST.get('category')
         amount = request.POST.get('amount')
         description = request.POST.get('description')
 
-        # Сохраняем новую категорию, если введена своя
+        # Проверка, если выбрана новая категория
         if request.POST.get('category') == 'custom' and category_name:
+            # Создаем новую категорию с указанным именем и типом
             category, created = Category.objects.get_or_create(name=category_name, type=transaction_type)
+        else:
+            # Убедитесь, что категория выбрана из выпадающего списка
+            category_id = request.POST.get('category')
+            if category_id:
+                category, created = Category.objects.get_or_create(name=category_id, type=transaction_type)
+            else:
+                # Обработка ошибки: категория не выбрана
+                return render(request, 'add_transaction.html', {
+                    'categories': categories,
+                    'error': 'Пожалуйста, выберите категорию.'
+                })
         first_user = User.objects.first()
         # Сохраняем транзакцию
         transaction = Transaction(
             user=first_user,
             date=date,
-            category=category if 'category' in locals() else None,
+            category=category,  # Используем категорию, которую мы получили или создали
             amount=amount,
-            comment=description
+            comment=description,
+             # Автоматически добавляем текущего пользователя
         )
         transaction.save()
 
-        return redirect('dashboard')  # Перенаправление на страницу со списком транзакций
+        return redirect('dashboard')
 
-    return render(request, 'addTransaction.html')
+    return render(request, 'add_transaction.html', {'categories': categories})
 
 def income(request):
     return render(request, 'main/income.html')
